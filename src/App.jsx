3f2d3b9ace06391521c571ce1f0435099204adfa
emmanuel sam <fndm4@gmail.com>
2026-02-13 09:40:18 +0100

    fixed sidebar active class issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import DashboardLayout from "./layout/DashboardLayout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import UserProfile from "./components/UserProfile";
 import Appointment from "./pages/Dashboard/Appointment";
-import Test from "./pages/Dashboard/Test";
 import Patients from "./pages/Dashboard/Patients";
 
 const App = () => {
@@ -18,7 +17,6 @@ const App = () => {
           <Route path="/dashboard/appointment" element={<Appointment />} />
           <Route path="/dashboard/patients" element={<Patients />} />
           <Route path="/user-profile" element={<UserProfile />} />
-          <Route path="/dashboard/test" element={<Test />} />
         </Route>
       </Routes>
     </>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -118,4 +118,5 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default React.memo(Dashboard);
+
