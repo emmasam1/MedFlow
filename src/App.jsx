95172e1b37c000377ebf754b952b0962a13524e8
unknown <ahmad.mjabdullah@gmail.com>
2026-02-13 17:17:35 +0100

    Patient Profile Created

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ import DashboardLayout from "./layout/DashboardLayout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import UserProfile from "./components/UserProfile";
 import Appointment from "./pages/Dashboard/Appointment";
-import Test from "./pages/Dashboard/Test";
 import Patients from "./pages/Dashboard/Patients";
-
 import Test from "./pages/Dashboard/Test";
+import PatientProfile from "./pages/Dashboard/PatientProfile";
 
 const App = () => {
   return (
@@ -21,6 +20,8 @@ const App = () => {
           <Route path="/dashboard/patients" element={<Patients />} />
           <Route path="/user-profile" element={<UserProfile />} />
           <Route path="/dashboard/test" element={<Test />} />
+          <Route path="/dashboard/patient-profile" element={<PatientProfile />} />
+          
         </Route>
       </Routes>
     </>
