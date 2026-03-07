b8d178b4c1ec528a3eef7ae7da7907f1a1212f18
emmanuel sam <fndm4@gmail.com>
2026-03-07 15:25:24 +0100

    did the edit patient

--- src/components/DoctorsAppointment.jsx
+++ src/components/DoctorsAppointment.jsx
@@ -265,7 +265,7 @@ const DoctorsAppointment = ({ onSuccess }) => {
             value={search}
             disabled={selectedPatient}
             onChange={(e) => setSearch(e.target.value)}
-            className="w-full capitalize px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none disabled:bg-gray-100 sentence-case"
+            className="w-full px-3 py-2 border border-gray-300 rounded-lg capitalize"
           />
 
           {selectedPatient && (
