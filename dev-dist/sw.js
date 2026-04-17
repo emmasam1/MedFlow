91606dadc615e77b905fb6b9cdba4d2bff57e271
unknown <ahmad.mjabdullah@gmail.com>
2026-04-17 23:51:14 +0100

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-0d7dea0d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.tsqnvraokk8"
+    "revision": "0.s5qbaf5prk4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
